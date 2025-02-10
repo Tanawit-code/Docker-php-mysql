048844fc73eb5d728406cb1cac25c39a7be5a0c1
Tanawit-code <freshjk24@gmail.com>
2025-02-10 08:10:22 +0700

    Update nav.php
    เพิ่ม from

--- php/src/nav.php
+++ php/src/nav.php
@@ -1,8 +1,27 @@
 <nav>
-    <ul>
+    <!-- <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Blog</a></li>
         <li><a href="#">Contact</a></li>
-    </ul>
+    </ul> -->
+
+    <?php  echo "เพิ่มฐานข้อมูล"  ?> 
+    
+    <form method="POST">
+    <label>First Name:</label>
+    <input type="text" name="first_name" required>
+    <br>
+    
+    <label>Last Name:</label>
+    <input type="text" name="last_name" required>
+    <br>
+    
+    <label>Age:</label>
+    <input type="number" name="age" required>
+    <br>
+    
+    <button type="submit" name="submit">Add User</button>
+</form>
+
 </nav>
